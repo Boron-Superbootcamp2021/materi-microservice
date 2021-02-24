e8191d407de4295fa770f80ab908702df2e26623
mfakhrinaufal <fakhrin23@gmail.com>
2021-02-24 21:50:07 +0700

    add validation id user

--- simple-moleculer/transaction-service.js
+++ simple-moleculer/transaction-service.js
@@ -14,29 +14,49 @@ brokerNodeTransaction.createService({
     settings: {
       fields: ["_id", "to", "from", "value"],
         entityValidator: {
-                  name: "string"
+                  to: "string",
+                  from: "string",
+                  value: "string"
               }
      },
   
     actions: {
         listTransaction: {
             async handler(ctx) {
-                this.broker.call("transaction.find", {}).then((res) => {
-                    this.broker.call("logger.create", {})
+                return this.broker.call("transaction.find", {}).then((res) => {
+                    await this.broker.call("loggers.createLog", { action: "Get transactions list", date: new Date()});
+                    console.log("Berhasil get", res);
+                    return res;
+                }).catch((err) => {
+                    console.log(err);
                 });
             }
         },
         createTransaction: {
+
             async handler(ctx) {
-                this.broker.call("transaction.create", ctx.params).then((res) => {
-                    this.broker.call("logger.create", {})
+                return this.broker.call("transaction.create", ctx.params).then((res) => {
+                    await this.broker.call("users.getUser", { id: ctx.params.id});
+                    await this.broker.call("users.getUser", { to: ctx.params.to});
+                    await this.broker.call("users.getUser", { from: ctx.params.from});
+                    await this.broker.call("loggers.createLog", { action: "Transaction", date: new Date()});
+                    console.log("Transaction success", res);
+                    return res;
+                }).catch((err) => {
+                    console.log(err);
+                    return err;
                 });;
             }
         },
         deleteTransaction: {
             async handler(ctx) {
-                this.broker.call("transaction.remove", { id }).then((res) => {
-                    this.broker.call("logger.create", {})
+                this.broker.call("transaction.remove", ctx.params).then((res) => {
+                    await this.broker.call("users.getUser",{id:ctx.params.id});
+                    await this.broker.call("loggers.createLog", { action: "Delete transaction", date: new Date() });
+                    console.log("berhasil Delete");
+                    return "Delete Berhasil";
+                }).catch((err) => {
+                    return (err);
                 });
             }
         }
